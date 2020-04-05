9fb771232bc2bc8b4fadc3bb413debd551bcb8f3
hwrnr <hawerner.hawerner@gmail.com>
2020-04-05 22:35:24 +0200

    Key length is bigger than str length

--- app/src/main/cpp/crypt.cpp
+++ app/src/main/cpp/crypt.cpp
@@ -5,7 +5,10 @@ void crypt(std::string &str, std::string key, std::string salt){
 	for (int i = 0; i < 512; ++i){
 		key = sha512(key + salt);
 	}
-	for (int i = 0, j = 0; i < str.length(); ++i, ++j){
-		str[i] = str[i] ^ key[j % key.length()];
+  	while (key.length() < str.length()){
+    	key += sha512(key + salt);
+  	}
+	for (unsigned long i = 0; i < str.length(); ++i){
+		str[i] = str[i] ^ key[i];
 	}
 }
